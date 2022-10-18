2fb4edd8eb68b2a5fc72e9d6f760f4267a97fcca
Mickael <106498339+mickael-f1@users.noreply.github.com>
2022-10-17 19:51:29 -0700

    Update theme.lua

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -24,8 +24,8 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(7)
-theme.border_width  = dpi(1)
+theme.useless_gap   = dpi(3)
+theme.border_width  = dpi(0)
 theme.border_normal = "#68538b"
 theme.border_focus  = "#e483c2"
 theme.border_marked = "#91231c"
@@ -104,7 +104,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = themes_path.."default/wallpaper.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
